5124c809c702c4c66487d1372175b851baeff8d3
engfernandes <gabriel.fernandes.06@outlook.com>
2022-08-30 18:06:03 -0300

    Unidade 2

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,3 +1,6 @@
+import { inspect } from "../decorators/inspect.js";
+import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
+
 export abstract class View<T> {
     private elemento: HTMLElement;
     private escapar = false;
@@ -16,6 +19,8 @@ export abstract class View<T> {
 
     protected abstract template(model: T): string;
 
+    @logarTempoDeExecucao(true)
+    @inspect()
     public update(model: T): void {
         let template = this.template(model);
         if(this.escapar) {
